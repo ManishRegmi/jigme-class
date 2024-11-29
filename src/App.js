bf8f97eda88f2771af2ccc43b10734c6a7ae6f75
Manish Regmi <manishregmi39@gmail.com>
2024-11-29 16:51:34 +0545

    first push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 
 function App() {
+  // const [color, setColor] = useState('red');
+  // const [height, setHeight] = useState('500px');
+  const [datas, setData] = useState([]);
+
+  useEffect(() => {
+    iAmLoading();
+  }, [])
+
+
+  const iAmLoading = async () => {
+    let result = await axios.get('https://66fcc56ec3a184a84d17ecf3.mockapi.io/vendors-near-me'); //promise (res, reject)
+    setData(result.data);
+  }
+
+
+  //map and forEach....
+
+  //async / await
+
+  // function squareOfNumber(fn, b) {
+  //   return fn(b); //callback function
+  // }
+
+  // function squareANo(number) {
+  //   console.log(number * number)
+  //   return number * number;
+  // }
+
+  // function cubeNumber(number){
+  //   console.log(number * number * number);
+
+  // }
+
+  // squareOfNumber(cubeNumber, 3)
+
+  // squareOfNumber(squareANo, 4)
+
+  // // useEffect()
+
+  // var a = [1, 2, 3]
+
+  // a.forEach((e, index) => {
+  //   if (e == 2) { 
+  //     a[index] = 4
+  //   }
+  // })
+
+  // console.log(a)
+
+
+
+  console.log(datas)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        datas.map((e) => {
+          return <div>
+            <img src={e.logo} height={"100px"} width={"100px"} />
+            <p>{e.phone}</p>
+            <p>{e.company}</p>
+            <p>{e.distanceAwayFromUser}</p>
+          </div>
+        })
+      }
     </div>
   );
 }
